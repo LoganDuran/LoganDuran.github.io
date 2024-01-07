63778d4f478a67fcf2cc18c3940cb656724a2407
LoganDuran <106428943+LoganDuran@users.noreply.github.com>
2024-01-07 17:42:59 +0100

    - mobile header implemented, - still working on further optimization - Мир Божији, Христос се роди! - Merry Christmas to all my Orthodox brothers and sisters.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,23 @@
+// Responsive Header
+
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const nav = document.querySelector(".nav");
+const header = document.querySelector(".header");
+
+hamburgerMenu.addEventListener("click", () => {
+    nav.classList.toggle("active")
+});
+
+hamburgerMenu.addEventListener("click", () => {
+    header.classList.toggle("active")
+});
+
+
+
+
+
+// MyProjects Expanding Images
+
 const panels = document.querySelectorAll(".panel");
 
 panels.forEach((panel) => {
