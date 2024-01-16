467a12732bfcbf15c79a36469259097ce18cbfe4
LoganDuran <106428943+LoganDuran@users.noreply.github.com>
2024-01-16 01:08:12 +0100

    started filling in the information

--- scripts.js
+++ scripts.js
@@ -3,6 +3,8 @@
 const hamburgerMenu = document.querySelector(".hamMenu");
 const nav = document.querySelector(".nav");
 const header = document.querySelector(".header");
+// const navLinks = document.querySelector(".navlinks");
+
 
 hamburgerMenu.addEventListener("click", () => {
     hamburgerMenu.classList.toggle("active")
@@ -16,6 +18,20 @@ hamburgerMenu.addEventListener("click", () => {
     header.classList.toggle("active")
 });
 
+
+// closing the header on anchor tags 
+
+// navLinks.addEventListener("click", () => {
+//     hamburgerMenu.classList.remove("active")
+// });
+
+// navLinks.addEventListener("click", () => {
+//     header.classList.remove("active")
+// });
+
+// navLinks.addEventListener("click", () => {
+//     nav.classList.remove("active")
+// });
 
 
 
