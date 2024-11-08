b9de1f6779892f6d459a1df98f168080fab93e39
LoganDuran <106428943+LoganDuran@users.noreply.github.com>
2024-11-08 21:35:25 +0100

    Implement animation in Tech Stack section; merge from "techstack-portfolio"

--- scripts.js
+++ scripts.js
@@ -5,33 +5,51 @@ const nav = document.querySelector(".nav");
 const header = document.querySelector(".header");
 const navLinks = document.querySelectorAll(".navlinks");
 
-
 hamburgerMenu.addEventListener("click", () => {
-    [header, nav, hamburgerMenu].forEach(section => section.classList.toggle("active"))
+  [header, nav, hamburgerMenu].forEach((section) =>
+    section.classList.toggle("active")
+  );
 });
-
 
 navLinks.forEach((navLink) => {
-    navLink.addEventListener("click", () => {
-        [header, nav, hamburgerMenu].forEach(section => section.classList.remove("active")) 
-    });
+  navLink.addEventListener("click", () => {
+    [header, nav, hamburgerMenu].forEach((section) =>
+      section.classList.remove("active")
+    );
+  });
 });
-
-
 
 // MyProjects Expanding Images
 
 const panels = document.querySelectorAll(".panel");
 
 panels.forEach((panel) => {
-    panel.addEventListener("click", () => {
-        removeActiveClass();
-        panel.classList.add("active");
-    });      
+  panel.addEventListener("click", () => {
+    removeActiveClass();
+    panel.classList.add("active");
+  });
 });
 
 const removeActiveClass = function () {
-    panels.forEach((panel) => {
-        panel.classList.remove("active");
-    });
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
 };
+
+// Dynamic TechStack Scripts
+
+function openTab(tabId) {
+  document.startViewTransition(() => {
+    const tabs = document.getElementsByClassName("popup-tab");
+    for (let i = 0; i < tabs.length; i++) {
+      tabs[i].style.display = "none";
+    }
+    document.getElementById(tabId).style.display = "grid";
+  });
+}
+
+function closeTab(element) {
+  document.startViewTransition(() => {
+    element.style.display = "none";
+  });
+}
